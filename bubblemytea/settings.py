5000ed2d6226a2c9ba72fdbe4f5356901f5626a3
nbr-23 <the-nbr23@protonmail.com>
2024-04-15 19:18:01 +0200

    [ADD] db.py, settings.py : database connection. Ref #14

--- bubblemytea/settings.py
+++ bubblemytea/settings.py
@@ -72,13 +72,18 @@ WSGI_APPLICATION = 'bubblemytea.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql11699181',
+        'USER': 'sql11699181',
+        'PASSWORD': 'ZfjSzeVCt2',
+        'HOST': 'sql11.freesqldatabase.com',
+        'PORT': '3306',
     }
 }
+"""
 
 
 # Password validation
