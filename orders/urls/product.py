55798a285196e545d4302818e15e6c38e7c44401
nbr-23 <the-nbr23@protonmail.com>
2024-04-16 16:16:22 +0200

    [ADD] product_add.html, product_add.py : adding products functions. Ref:#29

--- orders/urls/product.py
+++ orders/urls/product.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from orders.views.products import ProductListView
+from orders.views.product_add import ProductAddView
 
 urlpatterns = [
-    path('product/', ProductListView.as_view(), name='products_list')
+    path('product/', ProductListView.as_view(), name='products_list'),
+    path('product/add', ProductAddView.as_view(), name='product_add')
 ]
