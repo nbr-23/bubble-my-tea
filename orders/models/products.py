bdbd3e36cd697a4384949248b3fdf4f7187104a5
nbr-23 <the-nbr23@protonmail.com>
2024-04-16 13:59:00 +0200

    [DEBUG] settings.py, products.py : debbuging app from routes problem

--- orders/models/products.py
+++ orders/models/products.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 class Products(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
--- orders/views/products.py
+++ orders/views/products.py
@@ -1,11 +1,11 @@
 from django.views.generic import TemplateView
-
-
+from orders.models import products  # Assurez-vous d'importer le modèle Products
 
 class ProductListView(TemplateView):
-    template_name = 'order.html'
+    template_name = 'product_list.html'
 """
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['products'] = Products.objects.all()
-        return context"""
+        context['products'] = products.objects.all()
+        return context
+"""
