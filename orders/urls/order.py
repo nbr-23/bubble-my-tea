5df6dae99c5b035a0029e9c05d1309097183dc37
nbr-23 <the-nbr23@protonmail.com>
2024-04-17 23:16:04 +0200

    [ADD] adding order items ( panier ) functions. Ref#33

--- orders/urls/order.py
+++ orders/urls/order.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from orders.views.order import OrderView
+from orders.views.order_items import OrderItemsView
 from orders.views.dashboard import DashboardView
 from orders.views.admin import AdminView
 
 urlpatterns = [
-    path('order/', OrderView.as_view(), name='order'),
+    path('order/items', OrderItemsView.as_view(), name='order_items'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
--- orders/views/order.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import TemplateView
-
-class OrderView(TemplateView):
-    template_name = 'order.html'
