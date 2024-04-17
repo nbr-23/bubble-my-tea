2bf70df60c0b125565f831a8551e9c16219ba2e3
nbr-23 <the-nbr23@protonmail.com>
2024-04-17 12:56:07 +0200

    [ADD] product_show.html, product_show.py : page too see details of a product . Ref#48

--- orders/urls/product.py
+++ orders/urls/product.py
@@ -3,10 +3,12 @@ from orders.views.products import ProductListView
 from orders.views.product_add import ProductAddView
 from orders.views.product_update import ProductUpdateView
 from orders.views.product_delete import ProductDeleteView
+from orders.views.product_show import ProductShowView
 
 urlpatterns = [
     path('product/', ProductListView.as_view(), name='products_list'),
     path('product/add', ProductAddView.as_view(), name='product_add'),
+    path('product/show/<int:product_id>/', ProductShowView.as_view(), name='product_show'),
     path('product/update/<int:product_id>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:product_id>/', ProductDeleteView.as_view(), name='product_delete')
     
---
+++ orders/views/product_show.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+from orders.models.products import Products  
+from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render
+
+class ProductShowView(TemplateView):
+    template_name = 'product_show.html'
+
+    def get(self, request, product_id):
+        product = get_object_or_404(Products, id=product_id)
+        return render(request, self.template_name, {'product': product})
